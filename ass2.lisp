fd9e5a920981f0cf83b78513d3a3bffeeeebbccf
Adam Sanche <Adam Sanche>
2017-02-26 20:18:26 +0000

    beginning interpreter

--- ass2.lisp
+++ ass2.lisp
@@ -1,3 +1,24 @@
-(defun fl-interp (E P) 
-    
+(defun evaluate (E)
+    (cond 
+        ((equal (car E) 'first)
+            (caadr E))
+        ((equal (car E) 'rest)
+            (cdadr E))
+        (T E) ; like 'default' in Java switch
+    )
 )
+
+(defun fl-interp (E P)
+    (if (atom E)
+        E
+        (if (atom (car E))
+            (evaluate (list (car E) (fl-interp (cadr E) P)))
+            E ; else
+        )
+    )
+)
+
+(trace evaluate)
+(trace fl-interp)
+
+(fl-interp '(rest (1 2 (3))) nil)
