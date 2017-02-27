a6bd307ff8b3dde6f4d6dd7820c78baa5901cb4c
Adam Sanche <Adam Sanche>
2017-02-27 03:55:13 +0000

    addition, plus usefil comments showing what I've done

--- ass2.lisp
+++ ass2.lisp
@@ -6,6 +6,8 @@
             (cdadr E))
         ((equal (car E) 'eq)
             (eq (cadr E) (caddr E)))
+        ((equal (car E) '+)
+            (+ (cadr E) (caddr E)))
         (T E) ; like 'default' in Java switch
     )
 )
@@ -42,3 +44,25 @@
 (fl-interp '(first (rest (1 (2 3)))) nil)
 (fl-interp '(eq 2 2) nil)
 (fl-interp '(eq 2 1) nil)
+(fl-interp '(+ 4 1) nil)
+;; (fl-interp '(eq (< 3 4) (eq (+ 3 4) (- 2 3))) nil)
+
+;; (if x y z)
+;; (null x)
+;; (atom x)
+;; (eq x y) DONE
+;; (first x) DONE
+;; (rest x) DONE
+;; (cons x y)
+;; (equal x y)
+;; (isnumber x)  return T (true) if x is a number, NIL otherwise. 
+;;        Same as (numberp x) in Lisp
+;; (+ x y) DONE
+;; (- x y)
+;; (* x y)
+;; (> x y) 
+;; (< x y) 
+;; (= x y) 
+;; (and x y)
+;; (or x y)
+;; (not x)
