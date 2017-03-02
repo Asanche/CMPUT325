123f6ce10724be3def9dea68d8bf3f8a23b33950
asanche <asanche@ualberta.ca>
2017-03-02 01:35:51 +0000

    all test cases pass... #wewin

--- ass2.lisp
+++ ass2.lisp
@@ -2,6 +2,8 @@
 ;; Author: ADAM SANCHE
 ;; Date: March 1, 2017
 ;; Class: CMPUT 325 Lec B1
+;;;; NOTES
+;; I tested, and double tested all of the test cases, and unless I read them wrong, this works. :D
 ;;;;
 
 (defun sub-arg (E A) ;; of form ( (K . V) (K . V) ... )
@@ -158,52 +160,13 @@
     )
 )
 
-
-
-;; (print (fl-interp '(+ 10 5) nil)) ; > '15
-;; (print (fl-interp '(- 12 8) nil)) ; > '4
-;; (print (fl-interp '(* 5 9) nil)) ; > '45
-;; (print (fl-interp '(> 2 3) nil)) ; > 'nil
-;; (print (fl-interp '(< 1 131) nil)) ; > 't
-;; (print (fl-interp '(= 88 88) nil)) ; > 't
-;; (print (fl-interp '(and nil t) nil)) ; > 'nil
-;; (print (fl-interp '(or t nil) nil)) ; > 't
-;; (print (fl-interp '(not t) nil)) ; > 'nil
-;; (print (fl-interp '(isnumber 354) nil)) ; > 't
-;; (print (fl-interp '(equal (3 4 1) (3 4 1)) nil)) ; > 't
-;; (print (fl-interp '(if nil 2 3) nil)) ; > '3
-;; (print (fl-interp '(null ()) nil)) ; > 't
-;; (print (fl-interp '(atom (3)) nil)) ; > 'nil
-;; (print (fl-interp '(eq x x) nil)) ; > 't
-;; (print (fl-interp '(first (8 5 16)) nil)) ; > '8
-;; (print (fl-interp '(rest (8 5 16)) nil)) ; > '(5 16)
-;; (print (fl-interp '(cons 6 3) nil)) ; > '(6 . 3)
-
-;; (print (fl-interp '(+ (* 2 2) (* 2 (- (+ 2 (+ 1 (- 7 4))) 2))) nil)) ; > '12
-;; (print (fl-interp '(and (> (+ 3 2) (- 4 2)) (or (< 3 (* 2 2))) (not (= 3 2))) nil)) ; > 't
-;; (print (fl-interp '(or (= 5 (- 4 2)) (and (not (> 2 2)) (< 3 2))) nil)) ; > 'nil
-;; (print (fl-interp '(if (not (null (first (a c e)))) (if (isnumber (first (a c e))) (first (a c e)) (cons (a c e) d)) (rest (a c e))) nil)) ; > '((a c e) . d)
-
-;; (print (fl-interp '(greater 3 5) '((greater x y = (if (> x y) x (if (< x y) y nil)))))) ; > '5
-;; (print (fl-interp '(square 4) '((square x = (* x x))))) ; > '16
-;; (print (fl-interp '(simpleinterest 4 2 5) '((simpleinterest x y z = (* x (* y z)))))) ; > '40
-;; (print (fl-interp '(xor t nil) '((xor x y = (if (equal x y) nil t))))) ; > 't
-
-;; WFUCKING RECURSION!!!
 ;;;; USEFUL TRACE FUNCTIONS
 ;; (trace handle-p)
 ;; (trace handle-ps)
 ;; (trace handle-e)
 ;; (trace fl-ev)
-(trace fl-interp)
+;; (trace fl-interp)
 ;; (trace sub-args)
 ;; (trace sub-arg)
 ;; (trace get-args)
 ;; (trace get-func)
-
-;; (print (fl-interp '(last (s u p)) '((last x = (if (null (rest x)) (first x) (last (rest x))))))) ; > 'p **** ----- Recursion within the function
-;; (print (fl-interp '(push (1 2 3) 4) '((push x y = (if (null x) (cons y nil) (cons (first x) (push (rest x) y))))))) ; > '(1 2 3 4) ****
-;; (print (fl-interp '(pop (1 2 3)) '((pop x = (if (atom (rest (rest x))) (cons (first x) nil) (cons (first x)(pop (rest x)))))))) ; > '(1 2) ****
-;; (print (fl-interp '(power 4 2) '((power x y = (if (= y 1) x (power (* x x) (- y 1))))))) ; > '16 ****
-(print (fl-interp '(factorial 4) '((factorial x = (if (= x 1) 1 (* x (factorial (- x 1)))))))) ; > '24 ****
-;; (print (fl-interp '(divide 24 4) '((divide x y = (div x y 0)) (div x y z = (if (> (* y z) x) (- z 1) (div x y (+ z 1))))))) ; > '6 ****
