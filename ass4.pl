e883d670a64e34fcf89995f71604c54580bb84b1
Adam Sanche <Adam Sanche>
2017-04-14 03:08:07 +0100

    finish q1

--- ass4.pl
+++ ass4.pl
@@ -1,6 +1,13 @@
-count(N, N, N).
-count(N0, N1, N) :-
-   N1 > N0,
-   N = N1;
-   N2 is N1-1,
-   count(N0, N2, N).
+:- use_module(library(clpfd)).
+
+fourSquares(N, [S1, S2, S3, S4]):-
+    Vars = [S1, S2, S3, S4],
+    (S1*S1 + S2*S2 + S3*S3 + S4*S4) #= N,
+    S1 in 0..N,
+    S2 in 0..N,
+    S3 in 0..N,
+    S4 in 0..N,
+    S1 #=< S2,
+    S2 #=< S3,
+    S3 #=< S4,
+    label(Vars).
